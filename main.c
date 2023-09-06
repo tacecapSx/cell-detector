781de85845577d06131f2527c3d4012c3a89160a
tacecapSx <martin.handest@gmail.com>
2023-09-06 18:54:57 +0200

    rudimentary erosion, always black on image edge

--- main.c
+++ main.c
@@ -8,6 +8,21 @@
 #include <stdio.h>
 #include "cbmp.h"
 
+// Declaring variables
+  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
+  unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
+  unsigned char gray_image[BMP_WIDTH][BMP_HEIGHT];
+  unsigned char new_gray_image[BMP_WIDTH][BMP_HEIGHT];
+
+  #define EROSION_MASK_WIDTH 3
+  #define EROSION_MASK_HEIGHT 3
+
+  int erosion_mask[EROSION_MASK_WIDTH][EROSION_MASK_HEIGHT] = {
+    {0, 1, 0},
+    {1, 1, 1},
+    {0, 1, 0}
+  };
+
 void set_pixel(int x, int y, unsigned char val, unsigned char input_image[BMP_WIDTH][BMP_HEIGHT]){
   input_image[x][y] = val;
 }
@@ -65,9 +80,36 @@ void apply_threshold(unsigned char threshold, unsigned char input_image[BMP_WIDT
   }
 }
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
-  unsigned char gray_image[BMP_WIDTH][BMP_HEIGHT];
+void apply_erosion(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT], int erosion_mask[EROSION_MASK_WIDTH][EROSION_MASK_HEIGHT]) {
+  for(int x = 0; x < BMP_WIDTH; x++)
+  {
+    for(int y = 0; y < BMP_HEIGHT; y++)
+    {
+      for(int x_offset = 0; x_offset < EROSION_MASK_WIDTH; x_offset++)
+      {
+        for(int y_offset = 0; y_offset < EROSION_MASK_HEIGHT; y_offset++)
+        {
+          if(erosion_mask[x_offset][y_offset] == 1 && input_image[x - 1 + x_offset][y - 1 + y_offset] == 0) {
+            output_image[x][y] = 0;
+            goto exit;
+          }
+
+          output_image[x][y] = 255;
+        }
+      }
+      exit:
+      continue;
+    }
+  }
+}
+
+void save_grayscale_image(char file_name[10], unsigned char gray_image[BMP_WIDTH][BMP_HEIGHT], unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS]) {
+  // Convert back to RGB
+  convert_rgb(gray_image, rgb_image);
+
+  //Save image to file
+  write_bitmap(rgb_image, file_name);
+}
 
 //Main function
 int main(int argc, char** argv)
@@ -88,22 +130,34 @@ int main(int argc, char** argv)
 
   //Load image from file
   read_bitmap(argv[1], rgb_image);
+  write_bitmap(rgb_image, "Step1.bmp");
 
-  //Run inversion
+  // Convert to grayscale
   convert_grayscale(rgb_image,gray_image);
+  save_grayscale_image("Step2.bmp",gray_image, rgb_image);
 
+  // Set pixel test
   set_pixel(1,1,255,gray_image);
 
-  unsigned char c1 = get_pixel(1,1,gray_image);
-
-  printf("%hhu \n", c1);
-
+  // apply threshold
   apply_threshold(100, gray_image);
+  save_grayscale_image("Step3.bmp",gray_image, rgb_image);
 
-  convert_rgb(gray_image, rgb_image);
+  char file_name[11];
+  // Erode from step 4 onwards...
+  for(int s = 4; s < 14; s++) {
+    sprintf(file_name,"Step%d.bmp", s);
+    printf("%d\n", s);
 
-  //Save image to file
-  write_bitmap(rgb_image, argv[2]);
+    if(s % 2 == 1) {
+      apply_erosion(new_gray_image, gray_image, erosion_mask);
+      save_grayscale_image(file_name,gray_image, rgb_image);
+    }
+    else {
+      apply_erosion(gray_image, new_gray_image, erosion_mask);
+      save_grayscale_image(file_name,new_gray_image, rgb_image);
+    }
+  }
 
   printf("Done!\n");
   return 0;
