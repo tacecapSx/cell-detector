119c62faf04c90ecf2fa061e0fa1f85e95dc3200
tacecapSx <martin.handest@gmail.com>
2023-09-06 13:50:17 +0200

    implemented grayscale conversion, fix typo

--- main.c
+++ main.c
@@ -9,10 +9,10 @@
 #include "cbmp.h"
 
 //Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS]){
   for (int x = 0; x < BMP_WIDTH; x++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int y = 0; y < BMP_HEIGHT; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
@@ -22,9 +22,34 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
   }
 }
 
+void convert_grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT]) {
+  for(int x = 0; x < BMP_WIDTH; x++)
+  {
+    for(int y = 0; y < BMP_HEIGHT; y++)
+    {
+      unsigned char value = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+
+      output_image[x][y] = value;
+    }
+  }
+}
+
+void convert_rgb(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS]) {
+  for(int x = 0; x < BMP_WIDTH; x++)
+  {
+    for(int y = 0; y < BMP_HEIGHT; y++)
+    {
+      for(int c = 0; c < BMP_CHANNELS; c++)
+      {
+        output_image[x][y][c] = input_image[x][y];
+      }
+    }
+  }
+}
+
   //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+  unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
+  unsigned char gray_image[BMP_WIDTH][BMP_HEIGHT];
 
 //Main function
 int main(int argc, char** argv)
@@ -44,13 +69,15 @@ int main(int argc, char** argv)
   printf("Example program - 02132 - A1\n");
 
   //Load image from file
-  read_bitmap(argv[1], input_image);
+  read_bitmap(argv[1], rgb_image);
 
   //Run inversion
-  invert(input_image,output_image);
+  convert_grayscale(rgb_image,gray_image);
+
+  convert_rgb(gray_image, rgb_image);
 
   //Save image to file
-  write_bitmap(output_image, argv[2]);
+  write_bitmap(rgb_image, argv[2]);
 
   printf("Done!\n");
   return 0;
